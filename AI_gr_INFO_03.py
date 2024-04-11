83893c3937f72d9b8dd7f127cb0be27a8d1ce1b5
Mandarine061 <159799503+Mandarine061@users.noreply.github.com>
2024-04-11 11:59:27 +0200

    Add files via upload
    V0

---
+++ AI_gr_INFO_03.py
@@ -0,0 +1,63 @@
+#-*- coding: utf-8 -*-
+
+import math
+game = {}
+
+
+# other functions
+def analyze_game_state(game, player_id):
+    """
+    Analyze the current state of the game.
+
+    Parameters
+    ----------
+    game: game data structure (dict)
+    player_id: player id of AI (int)
+
+    Returns
+    -------
+    possible_actions : the different possible actions that the player should perform (list) 
+
+    """
+    
+
+def choose_action(possible_actions):
+    """
+    chooses the best action
+    
+    Paramaters
+    ----------
+    possible_actions : the different possible actions that the player should perform (list) 
+    
+    Retruns
+    -------
+    chosen_action : the action chosen by the AI (str)
+    """
+
+
+
+
+
+
+# main function - if necessary, other parameters can be used
+def get_AI_orders(chosen_action, player_id):
+    """Return orders of AI.
+    
+    Parameters
+    ----------
+    game: game data structure (dict)
+    player_id: player id of AI (int)
+
+    Returns
+    -------
+    orders: orders of AI (str)
+    
+    """
+
+    orders = ''
+    
+    ...
+    ...
+    ...
+    
+    return orders
