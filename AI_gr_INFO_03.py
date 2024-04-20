90dbc55c9d9a74bdeb3aca4b5cb30d4bc7f3fadc
Mandarine061 <159799503+Mandarine061@users.noreply.github.com>
2024-04-20 10:34:34 +0200

    Add files via upload

--- AI_gr_INFO_03.py
+++ AI_gr_INFO_03.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-import math
+import math, random
 game = {}
 
 
@@ -25,16 +25,7 @@ def choose_action(board_dictionary, players_dictionary, player_id):
     enemy_sheep = []
     seeds_pos = []
     enemy_grass = []
-    action = []
-
-    """
-    for position_x in range(int(board_dictionary['map'][0])):
-        forbidden_position.append([position_x, -1]) # 0 ???? parce que dans le BD on commence avec 0 mais si on change et qu'on commence avec 1 ALORS CHANGER ICI AUSSI 
-        forbidden_position.append([position_x, int(board_dictionary['map'][1])+1])
-    for position_y in range(int(board_dictionary['map'][1])):
-        forbidden_position.append([-1, position_y]) 
-        forbidden_position.append([int(board_dictionary['map'][0])+1, position_y])# NE PAS OUBLIER LES LIMITES AUX BORDS DU TABLEAU (LES COINS)
-    """    
+    action = [] 
 
     for rock in board_dictionary:
         forbidden_position.append([int(rock[0]), int(rock[1])])
@@ -42,7 +33,6 @@ def choose_action(board_dictionary, players_dictionary, player_id):
         forbidden_position.append([int(spawn[1]), int(spawn[2])])
     for seeds in board_dictionary:
         seeds_pos.append([int(seeds[0]), int(seeds[1])])
-    # ne pas oublier les limites de la map à mettre dans get ai orders, c'est elle qui va bouger le moutons de place
     for player in players_dictionary:
         if player == 'player_1':
             nb = 1
@@ -59,6 +49,8 @@ def choose_action(board_dictionary, players_dictionary, player_id):
 
     for our_sheep in owned_sheep: 
         for near_sheep in enemy_sheep:
+            if -8<=(our_sheep[0]-near_sheep[0])<=8 or -8<=(our_sheep[1]-near_sheep[1])<=8:
+                action.append('threat' + str([our_sheep][0]) + str([our_sheep][1]) + str([near_sheep][0]) + str([near_sheep][1])) 
             for forbidden_square in range(0, 5):
                 for forbidden_square_bis in range(0,5):
                     if [near_sheep[0]+forbidden_square, near_sheep[1]+forbidden_square_bis] not in forbidden_position:
@@ -74,29 +66,12 @@ def choose_action(board_dictionary, players_dictionary, player_id):
                         forbidden_position.append([near_sheep[0]-forbidden_square, near_sheep[1]-forbidden_square_bis])
         for grass in enemy_grass:
             if -8<=(our_sheep[0]-enemy_grass[0])<=8 or -8<=(our_sheep[1]-enemy_grass[1])<=8:
-                action.append('grass' + str([our_sheep][0]) + str([our_sheep][1]) + str([grass][0]) + str([grass][1])) 
+                action.append('grass ' + str([our_sheep][0]) + str([our_sheep][1]) + str([grass][0]) + str([grass][1])) 
         for seed in seeds_pos:
             if -8<=(our_sheep[0]-seed[0])<=8 or -8<=(our_sheep[1]-seed[1])<=8:
-                action.append('seed ' + str([our_sheep][0]) + str([our_sheep][1]) + str([seed][0]) + str([seed][1]))
+                action.append('seed  ' + str([our_sheep][0]) + str([our_sheep][1]) + str([seed][0]) + str([seed][1]))
 
     return [action, forbidden_position]
-            
-
-
-
-
-"""
-la fonction est censée regarder chaque mouton(boucle) et choisir la meilleure action possible en mettant une suite de if et elif afin que si par 
-exemple un mouton adverse est très proche, alors faut l'attaquer, en revanche, si c'est une case de graines qui est plus proche alors idéalement,
-déplacer le mouton vers cette case afin qu'il les broute et soit propriétaire de cette case
-
-ne pas oublier que on peut AJOUTER du codes aux squelettes 
-
-c'est le module de l'IA qu'on va importer sur le module du jeu
-"""
-
-
-
 
 
 # main function - if necessary, other parameters can be used
@@ -114,31 +89,29 @@ def get_AI_orders(board_dictionary, players_dictionary, player_id):
     orders: orders of AI (str)
     
     """
+    orders = ''
+    attack = random.randint(1, 2)
     actions = choose_action(board_dictionary, players_dictionary, player_id)
     action = actions[0]
     forbidden_position = actions[1]
     for sheep in players_dictionary['player_' + str(player_id)]['sheep']:
-        if 'seed ' in action or 'grass' in action:
-            if sheep[1] == [int(action[6]), int(action[7])]:
-                
-          for player in players_dictionary:
-                if player == 'player_1':
-                    nb = 1
-                else:
-                    nb = 2 
-    if nb == player_id:
-        if 'menaced' in action:
-            our_sheep = [sheep[8], sheep[9]] #en faire des listes our_sheep et enemy_sheep
-            enemy_sheep = [sheep[10], sheep[11]]
-
-          
-            
-
-
-        #for sheep in players_dictionary['player_' + str(player_id)]['sheep']: ????
-        #faire en sorte que notre mouton aille suffisament loin de l'autre (8 cases) et si au passage il y'a des trucs à manger dans les cases qui sont ok alors on y va 
-        #faire aussi en sorte queles alentours de tous les moutons ennemis soient en position interdite
-    ...
-    ...
-    
-    return action
+        if 'seed  ' in action:
+            if sheep[1] == [int(action[7]), int(action[8])] and [int(action[9]), int(action[10])] not in forbidden_position:
+                orders += str(action[7]) + '-' + str(action[8]) + ':@' + str(action[9]) + '-' + str(action[10]) + ' '
+        elif 'grass ' in action:
+            if sheep[1] == [int(action[7]), int(action[8])] and [int(action[9]), int(action[10])] not in forbidden_position:
+                orders += str(action[7]) + '-' + str(action[8]) + ':*' + str(action[9]) + '-' + str(action[10]) + ' '
+        elif 'threat' in action and attack == 1:
+            orders += str(action[7]) + '-' + str(action[8]) + ':x' + str(action[9]) + '-' + str(action[10]) + ' '
+        else:
+            while sheep[1] == sheep[1]:
+                new_pos_x = random.randint(1,8)
+                new_pos_y = random.randint(1,8)
+                if ([sheep[1][0] + new_pos_x, sheep[1][1] + new_pos_y] not in forbidden_position 
+                    and (sheep[1][0] + new_pos_x) >= int(board_dictionary['map'][0]) and (sheep[1][1] + new_pos_y) >= int(board_dictionary['map'][1])):
+                    ex_pos_x = sheep[1][0]
+                    ex_pos_y = sheep[1][1]
+                    sheep[1][0] += new_pos_x
+                    sheep[1][1] += new_pos_y
+                    orders += str(ex_pos_x) + '-' + str(ex_pos_y) + ':@' + str(sheep[1][0]) + '-' + str(sheep[1][1]) + ' '
+    return orders
