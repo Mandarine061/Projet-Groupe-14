e8f4fba09531551daccd69954d8ce0d67445f1d3
Mandarine061 <159799503+Mandarine061@users.noreply.github.com>
2024-04-22 21:37:12 +0200

    Add files via upload

--- AI_gr_INFO_03.py
+++ AI_gr_INFO_03.py
@@ -8,7 +8,7 @@ game = {}
 
 def choose_action(board_dictionary, players_dictionary, player_id):
     """
-    chooses the best action
+    chooses the best action.
     
     Paramaters
     ----------
@@ -35,6 +35,7 @@ def choose_action(board_dictionary, players_dictionary, player_id):
         forbidden_position.append([int(spawn[1]), int(spawn[2])])
     for seeds in board_dictionary['seeds']:
         seeds_pos.append([int(seeds[0]), int(seeds[1])])
+
     for player in players_dictionary:
         if player == 'player_1':
             nb = 1
@@ -51,11 +52,11 @@ def choose_action(board_dictionary, players_dictionary, player_id):
 
     for our_sheep in owned_sheep: 
             for near_sheep in enemy_sheep:
-                if -8<=(our_sheep[1][0]-near_sheep[1][0])<=8 or -8<=(our_sheep[1][1]-near_sheep[1][1])<=8 and our_sheep[1] not in already_executed:
+                if -8<=(our_sheep[1][0]-near_sheep[1][0])<=8 and -8<=(our_sheep[1][1]-near_sheep[1][1])<=8 and our_sheep[1] not in already_executed:
                     action.append(['threat',[str(our_sheep[1][0]), str(our_sheep[1][1])], [str(near_sheep[1][0]), str(near_sheep[1][1])]])
                     already_executed.append(our_sheep[1])
-                for forbidden_square in range(0, 5):
-                    for forbidden_square_bis in range(0,5):
+                for forbidden_square in range(0, 9):
+                    for forbidden_square_bis in range(0,9):
                         if [near_sheep[1][0]+forbidden_square, near_sheep[1][1]+forbidden_square_bis] not in forbidden_position:
                             forbidden_position.append([near_sheep[1][0]+forbidden_square, near_sheep[1][1]+forbidden_square_bis])
 
@@ -68,11 +69,13 @@ def choose_action(board_dictionary, players_dictionary, player_id):
                         if [near_sheep[1][0]-forbidden_square, near_sheep[1][1]-forbidden_square_bis] not in forbidden_position:
                             forbidden_position.append([near_sheep[1][0]-forbidden_square, near_sheep[1][1]-forbidden_square_bis])
             for grass in enemy_grass:
-                if -8<=(our_sheep[1][0]-grass[1][0])<=8 or -8<=(our_sheep[1][1]-grass[1][1])<=8 and our_sheep[1] not in already_executed:
+                if -8<=(our_sheep[1][0]-grass[1][0])<=8 and -8<=(our_sheep[1][1]-grass[1][1])<=8 and our_sheep[1] != grass[1] and our_sheep[1] not in already_executed:
                     action.append(['grass', [str(our_sheep[1][0]), str(our_sheep[1][1])], [str(grass[1][0]), str(grass[1][1])]]) 
                     already_executed.append(our_sheep[1])
+                if our_sheep[1] == grass[1]:
+                    action.append(['graze', [str(our_sheep[1][0]), str(our_sheep[1][1])]])
             for seed in seeds_pos:
-                if -8<=(our_sheep[1][0]-seed[0])<=8 or -8<=(our_sheep[1][1]-seed[1])<=8 and our_sheep[1] not in already_executed:
+                if -8<=(our_sheep[1][0]-seed[0])<=8 and -8<=(our_sheep[1][1]-seed[1])<=8 and our_sheep[1] not in already_executed:
                     action.append(['seed', [str(our_sheep[1][0]), str(our_sheep[1][1])], [str(seed[0]), str(seed[1])]])
                     already_executed.append(our_sheep[1])
 
@@ -96,53 +99,50 @@ def get_AI_orders(board_dictionary, players_dictionary, player_id):
     
     """
     orders = ''
-    attack = random.randint(1, 2)
-    action_forb = choose_action(board_dictionary, players_dictionary, player_id)
-    actions = action_forb[0]
-    forbidden_position = action_forb[1]
-    #
+    attack =  random.randint(1, 2)
+    action_forbidden_pos = choose_action(board_dictionary, players_dictionary, player_id)
+    actions = action_forbidden_pos[0]
+    forbidden_position = action_forbidden_pos[1]
+    new_pos_x = random.randint(-8,9)
+    new_pos_y = random.randint(-8,9)
+    if len(players_dictionary['player_' + str(player_id)]['grass']) >= 30 and len(players_dictionary['player_' + str(player_id)]['sheep']) < 2:
+        orders += 'sheep '
+    if len(players_dictionary['player_' + str(player_id)]['grass']) >= 60 and len(players_dictionary['player_' + str(player_id)]['sheep']) < 3:
+        orders += 'sheep '
+    if len(players_dictionary['player_' + str(player_id)]['grass']) >= 90 and len(players_dictionary['player_' + str(player_id)]['sheep']) < 4:
+        orders += 'sheep '
+
     for action in actions:
         for sheep in players_dictionary['player_' + str(player_id)]['sheep']:
+            ex_pos_x = sheep[1][0]
+            ex_pos_y = sheep[1][1]
             if 'seed' in action:
-                if sheep[1] == action[1] and action[2] not in forbidden_position:
-                    print('1')
+                if action[2] not in forbidden_position and action[1][0] + '-' + action[1][1] not in orders:
                     orders += action[1][0] + '-' + action[1][1] + ':@' + action[2][0] + '-' + action[2][1] + ' '
-            elif 'grass' in action:
-                if sheep[1] == action[1] and action[2] not in forbidden_position:
-                    print('2')
-                    orders +=  action[2][0] + '-' + action[2][1] + ':*' + ' '
-            elif 'threat' in action and attack == 1:
-                print('3')
+            if 'grass' in action:
+                if action[2] not in forbidden_position and action[1][0] + '-' + action[1][1] not in orders:
+                    orders += action[1][0] + '-' + action[1][1] + ':@' + action[2][0] + '-' + action[2][1] + ' '
+            elif 'graze' in action:
+                if action[1][0] + '-' + action[1][1] not in orders:
+                    orders +=  action[1][0] + '-' + action[1][1] + ':*' + ' '
+            elif 'threat' in action and attack == 1 and action[1][0] + '-' + action[1][1] not in orders:
                 orders += action[1][0] + '-' + action[1][1] + ':x' + action[2][0] + '-' + action[2][1] + ' '
-            else:
-                print('4')           
-                new_pos_x = random.randint(-8,9)
-                new_pos_y = random.randint(-8,9)
-                if ([sheep[1][0] + new_pos_x, sheep[1][1] + new_pos_y] not in forbidden_position 
-                    and (sheep[1][0] + new_pos_x) >= int(board_dictionary['map'][0]) and (sheep[1][1] + new_pos_y) >= int(board_dictionary['map'][1])):
-                    ex_pos_x = sheep[1][0]
-                    ex_pos_y = sheep[1][1]
-                    sheep[1][0] += new_pos_x
-                    sheep[1][1] += new_pos_y
+            elif 'threat' in action and attack == 2 and str(ex_pos_x) + '-' + str(ex_pos_y) not in orders:         
+                if ([sheep[1][0] + new_pos_x, sheep[1][1] + new_pos_y] not in forbidden_position):
+                    if sheep[1][0] + new_pos_x < 0:
+                        sheep[1][0] = 0
+                    elif sheep[1][0] + new_pos_x > int(board_dictionary['map'][0]):
+                        sheep[1][0] = int(board_dictionary['map'][0])
+                    else:
+                        sheep[1][0] += new_pos_x
+                    if sheep[1][1] + new_pos_y < 0:
+                        sheep[1][1] = 0
+                    elif sheep[1][1] + new_pos_y > int(board_dictionary['map'][1]):
+                        sheep[1][0] = int(board_dictionary['map'][1])
+                    else:
+                        sheep[1][1] += new_pos_y
                     orders += str(ex_pos_x) + '-' + str(ex_pos_y) + ':@' + str(sheep[1][0]) + '-' + str(sheep[1][1]) + ' '
     return orders
 
-board_dictionary = {'rocks':[['1','2'], ['3','6'], ['9','11'], ['4','16']], 
-                    'map':['20','20'], 
-                    'spawn':[['1','12','5'], ['2','6','8']],
-                    'seeds':[['13','13'], ['3', '4'], ['7','8'], ['9','8']]}
-             
-             
-players_dictionary = {'player_1':{'sheep':[[1, [4 ,6]], [3, [6, 6]]],
-                      'grass': [[2, [4, 12]], [2, [5, 12]], [2, [4, 11]], [2, [4, 13]]]},
-                      'player_2':{'sheep':[[2, [15, 17]], [2, [6, 12]]],
-                      'grass': [[11, [16, 13]], [1, [16, 12]], [1, [17, 13]], [1, [15, 13]], [1, [16, 14]]]}}
 
-#print(get_AI_orders(board_dictionary, players_dictionary, 1))
-print(choose_action(board_dictionary, players_dictionary, 1))
 
-"""
-ex_pos_x = sheep[1][0]
-            ex_pos_y = sheep[1][1]
-            while [ex_pos_x, ex_pos_y] == sheep[1]:
-            """
